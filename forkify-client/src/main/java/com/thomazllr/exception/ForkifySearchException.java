92b5737c67484599e8183c6385f118d03faa7434
thomazllr <guithomaz270@gmail.com>
2025-10-05 06:40:04 -0300

    feat: aplicando pom.xml pai para definir o projeto como modulos e ficando de fácil organização

--- forkify-client/src/main/java/com/thomazllr/exception/ForkifySearchException.java
+++ forkify-client/src/main/java/com/thomazllr/exception/ForkifySearchException.java
@@ -2,7 +2,7 @@ package com.thomazllr.exception;
 
 public class ForkifySearchException extends RuntimeException {
 
-    public ForkifySearchException(String message) {
-        super("No recipes found for '%s'. See phrases at 'http://forkify-api.herokuapp.com/phrases.html'".formatted(message));
+    public ForkifySearchException(String query) {
+        super("No recipes found for '%s'. See phrases at 'http://forkify-api.herokuapp.com/phrases.html'".formatted(query));
     }
 }
